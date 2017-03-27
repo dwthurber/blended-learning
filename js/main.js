aae0535c50afd541f118f8a4c377eb81f601e575
Derek T <fbfb1694@opayq.com>
2017-03-27 14:58:03 +0000

    redesign

--- js/main.js
+++ js/main.js
@@ -20,7 +20,13 @@ jQuery(document).ready(function($){
 	});
 	gallery.on('click', function(event){
 		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
-		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
+		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) {
+            toggleContent('', false);
+            $('.cd-nav-trigger').delay(1000).queue(function(next){
+                $(this).removeClass("hidden");
+                next();
+            });
+        } 
 	})
 
 	function openItemInfo(url) {
@@ -94,46 +100,13 @@ jQuery(document).ready(function($){
 			});
 		}
 	});
-  var introSection = $('#cd-intro-background'),
-		introSectionHeight = introSection.height(),
-		//change scaleSpeed if you want to change the speed of the scale effect
-		scaleSpeed = 0.3,
-		//change opacitySpeed if you want to change the speed of opacity reduction effect
-		opacitySpeed = 1; 
-	
-	//update this value if you change this breakpoint in the style.css file (or _layout.scss if you use SASS)
-	var MQ = 1170;
-
-	triggerAnimation();
-	$(window).on('resize', function(){
-		triggerAnimation();
-	});
-
-	//bind the scale event to window scroll if window width > $MQ (unbind it otherwise)
-	function triggerAnimation(){
-		if($(window).width()>= MQ) {
-			$(window).on('scroll', function(){
-				//The window.requestAnimationFrame() method tells the browser that you wish to perform an animation- the browser can optimize it so animations will be smoother
-				window.requestAnimationFrame(animateIntro);
-			});
-		} else {
-			$(window).off('scroll');
-		}
-	}
-	//assign a scale transformation to the introSection element and reduce its opacity
-	function animateIntro () {
-		var scrollPercentage = ($(window).scrollTop()/introSectionHeight).toFixed(5),
-			scaleValue = 1 - scrollPercentage*scaleSpeed;
-		//check if the introSection is still visible
-		if( $(window).scrollTop() < introSectionHeight) {
-			introSection.css({
-			    '-moz-transform': 'scale(' + scaleValue + ') translateZ(0)',
-			    '-webkit-transform': 'scale(' + scaleValue + ') translateZ(0)',
-				'-ms-transform': 'scale(' + scaleValue + ') translateZ(0)',
-				'-o-transform': 'scale(' + scaleValue + ') translateZ(0)',
-				'transform': 'scale(' + scaleValue + ') translateZ(0)',
-				'opacity': 1 - scrollPercentage*opacitySpeed
-			});
-		}
-	}
+    
+    $('.header-more').on('click', function(event){
+        $('body,html').animate({
+            'scrollTop': gallery.offset().top
+        });
+    });
+    
+//    Initiate lightgallery
+    $("#lightgallery").lightGallery();
 });
